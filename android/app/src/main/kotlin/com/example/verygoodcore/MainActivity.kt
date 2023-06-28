a6132ea964069b0599120fac712f822dcbb2d38c
Nico <102190013+NicoCieri10@users.noreply.github.com>
2023-06-27 23:11:54 -0300

    create project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.clever_buddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:clever_buddy/app/app.dart';
+import 'package:clever_buddy/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
