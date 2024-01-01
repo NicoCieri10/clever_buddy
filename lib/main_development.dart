eacf0fbdcfc3bc2d3791673e6f7c39e81954b0ac
Nico <102190013+NicoCieri10@users.noreply.github.com>
2024-01-01 17:30:06 -0300

    feat: supabase instance

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,17 @@
 import 'package:clever_buddy/app/app.dart';
 import 'package:clever_buddy/bootstrap.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  bootstrap(() => const PageApp());
+Future<void> main() async {
+  const supabaseUrl = 'https://msmcnygourjaqbewrqnq.supabase.co';
+  const supabaseKey =
+      // ignore: lines_longer_than_80_chars
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbWNueWdvdXJqYXFiZXdycW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQxNDA0NTksImV4cCI6MjAxOTcxNjQ1OX0.BPw6dprHOLkF9YA74s6O1xnnojgx9KDdngv9zdtepfQ';
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
+
+  await bootstrap(() => const PageApp());
 }
